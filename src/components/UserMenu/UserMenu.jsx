dd7e7948dbdffece072c6ef9d414cf09d62b5ea5
DON <noone2777@gmail.com>
2024-11-12 20:44:50 +0200

    final fix

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,19 +1,22 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import css from './userMenu.module.css';
 import { apiLogout } from '../../redux/auth/operations';
+import { selectUserName } from '../../redux/auth/selectors';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const onLogout = () => {
     dispatch(apiLogout());
   };
+  const name = useSelector(selectUserName);
   return (
-    <>
+    <div className={css.container}>
+      <h3 className={css.welcomeMessage}>Welcome, {name}</h3>
       <button type="button" onClick={onLogout} className={css.logoutBtn}>
         LogOut
       </button>
-    </>
+    </div>
   );
 };
 
